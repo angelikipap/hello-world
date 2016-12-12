b50e745f60339cef8983a6b699afa5212f115941
apapaioannou <apapaioannou>
2016-12-12 17:14:36 +0200

    update on master

--- hello-world/src/MainClass.java
+++ hello-world/src/MainClass.java
@@ -3,7 +3,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("Hello world1asasds");
+
+		System.out.println("Hello world2");
+
 	}
 
 }
